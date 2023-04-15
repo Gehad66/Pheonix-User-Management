54cbda4fef905833c9cb04b139ad3720d2689909
Gehad66 <26491862+Gehad66@users.noreply.github.com>
2023-04-16 01:36:44 +0200

    Refactored Execution Factory

--- src/main/java/com/spotlight/platform/userprofile/api/model/operations/ExecuteOperation.java
+++ src/main/java/com/spotlight/platform/userprofile/api/model/operations/ExecuteOperation.java
@@ -2,7 +2,11 @@ package com.spotlight.platform.userprofile.api.model.operations;
 
 import com.spotlight.platform.userprofile.api.core.request.OperationRequest;
 import com.spotlight.platform.userprofile.api.core.request.OperationResponse;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyName;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyValue;
+
+import java.util.Map;
 
 public interface ExecuteOperation {
-    OperationResponse execute(OperationRequest operationRequest);
+    Map<UserProfilePropertyName, UserProfilePropertyValue> execute(OperationRequest operationRequest, Map<UserProfilePropertyName, UserProfilePropertyValue> oldProperties);
 }
