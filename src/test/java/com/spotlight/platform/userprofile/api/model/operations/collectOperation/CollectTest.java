d54f0bd11aafc329970252682938663a6aa7e7fa
Gehad66 <26491862+Gehad66@users.noreply.github.com>
2023-04-16 01:43:56 +0200

    Update userProfile operations & testcases

--- src/test/java/com/spotlight/platform/userprofile/api/model/operations/collectOperation/CollectTest.java
+++ src/test/java/com/spotlight/platform/userprofile/api/model/operations/collectOperation/CollectTest.java
@@ -1,12 +1,58 @@
 package com.spotlight.platform.userprofile.api.model.operations.collectOperation;
 
+import com.spotlight.platform.userprofile.api.core.enums.OperationTypesEnum;
+import com.spotlight.platform.userprofile.api.core.exceptions.OperationValidationException;
+import com.spotlight.platform.userprofile.api.core.request.OperationRequest;
+import com.spotlight.platform.userprofile.api.model.operations.ExecuteOperationFactory;
+import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyName;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyValue;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfileFixtures.LAST_UPDATE_TIMESTAMP;
+import static com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfileFixtures.USER_ID;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CollectTest {
-//TODO
-//    @Test
-//    void execute() {
-//    }
+
+    UserProfile userProfile = new UserProfile(USER_ID, LAST_UPDATE_TIMESTAMP,
+    Map.of(UserProfilePropertyName.valueOf("inventory"), UserProfilePropertyValue.valueOf(new String[] {"inventory", "inventory2"}),
+                UserProfilePropertyName.valueOf("tools"), UserProfilePropertyValue.valueOf(new String[] {"tools", "tools2"})));
+    UserProfilePropertyName castString(String property){
+        return UserProfilePropertyName.valueOf(property);
+    }
+    @Test
+    void collect_valid_operation() {
+        ExecuteOperationFactory operationFactory = new ExecuteOperationFactory();
+        Map<UserProfilePropertyName, UserProfilePropertyValue> oldProperties = userProfile.userProfileProperties();
+        Map<UserProfilePropertyName, UserProfilePropertyValue> newProperties = new HashMap<>();
+        newProperties.put(UserProfilePropertyName.valueOf("inventory"), UserProfilePropertyValue.valueOf(new String[] {"sword1", "sword2"}));
+        newProperties.put(UserProfilePropertyName.valueOf("tools"), UserProfilePropertyValue.valueOf(new String[] {"tools3", "tools4"}));
+
+        OperationRequest operationRequest = new OperationRequest(USER_ID, OperationTypesEnum.COLLECT, newProperties);
+        Map<UserProfilePropertyName, UserProfilePropertyValue> result = operationFactory.execute(operationRequest, oldProperties);
+
+        UserProfilePropertyValue ExpectedInventoryOutput = UserProfilePropertyValue.valueOf( Arrays.asList(
+                new String[] {"inventory", "inventory2", "sword1", "sword2"}));
+        UserProfilePropertyValue ExpectedToolsOutput = UserProfilePropertyValue.valueOf( Arrays.asList(
+                new String[] {"tools", "tools2", "tools3", "tools4"}));
+
+        assertThat(result.get(castString("inventory")).equals(ExpectedInventoryOutput)).isTrue();
+        assertThat(result.get(castString("tools")).equals(ExpectedToolsOutput)).isTrue();
+    }
+    @Test
+    void non_existing_user_property() {
+        ExecuteOperationFactory operationFactory = new ExecuteOperationFactory();
+        Map<UserProfilePropertyName, UserProfilePropertyValue> oldProperties = userProfile.userProfileProperties();
+        Map<UserProfilePropertyName, UserProfilePropertyValue> newProperties = new HashMap<>();
+        newProperties.put(UserProfilePropertyName.valueOf("non-existing"), UserProfilePropertyValue.valueOf(new String[] {"sword1", "sword2"}));
+        OperationRequest operationRequest = new OperationRequest(USER_ID, OperationTypesEnum.COLLECT, newProperties);
+        assertThrows(OperationValidationException.class, () -> {
+            operationFactory.execute(operationRequest, oldProperties);}  );
+    }
 }
--- src/test/java/com/spotlight/platform/userprofile/api/model/operations/replaceOperation/ReplaceTest.java
+++ src/test/java/com/spotlight/platform/userprofile/api/model/operations/replaceOperation/ReplaceTest.java
@@ -1,12 +1,45 @@
 package com.spotlight.platform.userprofile.api.model.operations.replaceOperation;
 
+import com.spotlight.platform.userprofile.api.core.enums.OperationTypesEnum;
+import com.spotlight.platform.userprofile.api.core.exceptions.OperationValidationException;
+import com.spotlight.platform.userprofile.api.core.request.OperationRequest;
+import com.spotlight.platform.userprofile.api.model.operations.ExecuteOperationFactory;
+import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyName;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyValue;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfileFixtures.LAST_UPDATE_TIMESTAMP;
+import static com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfileFixtures.USER_ID;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReplaceTest {
-//TODO
-//    @Test
-    void execute() {
+    UserProfile userProfile = new UserProfile(USER_ID, LAST_UPDATE_TIMESTAMP,
+            Map.of(UserProfilePropertyName.valueOf("currentGold"), UserProfilePropertyValue.valueOf(100),
+                    UserProfilePropertyName.valueOf("currentGems"), UserProfilePropertyValue.valueOf(10)));
+    @Test
+    void replace_valid() {
+        ExecuteOperationFactory operationFactory = new ExecuteOperationFactory();
+        Map<UserProfilePropertyName, UserProfilePropertyValue> oldProperties = userProfile.userProfileProperties();
+        Map<UserProfilePropertyName, UserProfilePropertyValue> newProperties = new HashMap<>();
+        newProperties.put(UserProfilePropertyName.valueOf("currentGold"), UserProfilePropertyValue.valueOf(500));
+        newProperties.put(UserProfilePropertyName.valueOf("currentGems"), UserProfilePropertyValue.valueOf(2));
+        OperationRequest operationRequest = new OperationRequest(USER_ID, OperationTypesEnum.REPLACE, newProperties);
+        UserProfilePropertyValue result = operationFactory.execute(operationRequest, oldProperties).get(UserProfilePropertyName.valueOf("currentGold"));
+        assertThat(result).isEqualTo(UserProfilePropertyValue.valueOf(500));
+    }
+    @Test
+    void non_existing_user_property() {
+        ExecuteOperationFactory operationFactory = new ExecuteOperationFactory();
+        Map<UserProfilePropertyName, UserProfilePropertyValue> oldProperties = userProfile.userProfileProperties();
+        Map<UserProfilePropertyName, UserProfilePropertyValue> newProperties = new HashMap<>();
+        newProperties.put(UserProfilePropertyName.valueOf("non-existing"), UserProfilePropertyValue.valueOf(-10));
+        OperationRequest operationRequest = new OperationRequest(USER_ID, OperationTypesEnum.REPLACE, newProperties);
+        assertThrows(OperationValidationException.class, () -> {
+            operationFactory.execute(operationRequest, oldProperties);}  );
     }
 }
