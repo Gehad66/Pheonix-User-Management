1b67f707b5ab955f62d688242acedd73e75e712c
Gehad66 <26491862+Gehad66@users.noreply.github.com>
2023-04-17 06:53:30 +0200

    Update UserProfilePropertyMap.java
    fixing deep copy issue

--- src/main/java/com/spotlight/platform/userprofile/api/model/profile/primitives/UserProfilePropertyMap.java
+++ src/main/java/com/spotlight/platform/userprofile/api/model/profile/primitives/UserProfilePropertyMap.java
@@ -2,7 +2,6 @@ package com.spotlight.platform.userprofile.api.model.profile.primitives;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,18 +9,16 @@ public class UserProfilePropertyMap {
     public Map<UserProfilePropertyName, UserProfilePropertyValue> userProfileProperties;
     @JsonCreator
     public UserProfilePropertyMap(Map<UserProfilePropertyName, UserProfilePropertyValue> userProfileProperties) {
-        this.userProfileProperties = userProfileProperties;
+        this.userProfileProperties = new HashMap<UserProfilePropertyName, UserProfilePropertyValue>(userProfileProperties);
     }
     public UserProfilePropertyMap() {
         this.userProfileProperties = new HashMap<>();
     }
+    @JsonValue
+    protected Map<UserProfilePropertyName, UserProfilePropertyValue> getValue() {
+        return userProfileProperties;
+    }
     public static UserProfilePropertyMap valueOf(Map<UserProfilePropertyName, UserProfilePropertyValue> userProfileProperties) {
         return new UserProfilePropertyMap(userProfileProperties);
     }
-    @JsonValue
-    protected Map<UserProfilePropertyName, UserProfilePropertyValue> getPropertyMap() {
-        return userProfileProperties;
-    }
-
-
 }
